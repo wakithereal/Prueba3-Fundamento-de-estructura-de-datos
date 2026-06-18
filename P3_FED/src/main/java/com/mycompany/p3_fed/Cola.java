1df9a44cb7c4f24c9c96e2fa084285a5effa733e
Joaquín Wylie <joaquin.wylie@cloud.uautonoma.cl>
2026-06-17 20:19:59 -0400

    Cola dinamica para registrar reclamos

--- P3_FED/src/main/java/com/mycompany/p3_fed/Cola.java
+++ P3_FED/src/main/java/com/mycompany/p3_fed/Cola.java
@@ -8,6 +8,61 @@ package com.mycompany.p3_fed;
  *
  * @author wakictm
  */
+//Cola dinamica vamoooooooooooooooooo
+
 public class Cola {
     
+
+    private Reclamo[] reclamos;
+    private int fin, inicio;
+ 
+    
+    public Cola(){
+    
+        reclamos = new Reclamo[0];
+        fin = -1;
+        inicio = 0;
+    }
+    
+    public void agregar(Reclamo reclamo){
+    
+       
+            Reclamo[] temp = reclamos;
+            
+            reclamos = new Reclamo[reclamos.length+1];
+            
+            for( int i = 0; i < temp.length; i++){
+            
+                reclamos[i] = temp[i];
+            
+            }
+            
+        
+        
+        reclamos[++fin] = reclamo;
+        
+    }
+    
+    public Reclamo eliminar(){
+    
+        if(fin == -1){
+            
+            System.out.println("No hay elementos en la cola");
+            return null;
+    }
+        
+        return reclamos[++inicio];
+
 }
+    
+    public void mostrarReclamos(){
+    
+        for( int i = inicio; i <= fin; i++ ){
+        
+            System.out.println((i+1)+". "+reclamos[i]);
+        }
+        System.out.println("");
+    }
+
+
+   }
