a1b6a76a6b074ad4db817110101b6bfc3220d3ec
Joaquín Wylie <joaquin.wylie@cloud.uautonoma.cl>
2026-06-17 20:17:54 -0400

    Iniciando proyecto

---
+++ P3_FED/src/main/java/com/mycompany/p3_fed/P3_FED.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.p3_fed;
+
+/**
+ *
+ * @author wakictm
+ */
+public class P3_FED {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
