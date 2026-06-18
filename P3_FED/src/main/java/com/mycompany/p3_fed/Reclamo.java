a777aca3edd42697572575fa342b67ffc17916bb
Joaquín Wylie <joaquin.wylie@cloud.uautonoma.cl>
2026-06-17 20:19:07 -0400

    Inicializando las clases

---
+++ P3_FED/src/main/java/com/mycompany/p3_fed/Reclamo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.p3_fed;
+
+/**
+ *
+ * @author wakictm
+ */
+public class Reclamo {
+    
+}
